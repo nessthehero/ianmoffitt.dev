101b30a79db145d0341ac2f885916d2e78e2b3b4
Ian Moffitt <nessthehero@gmail.com>
2014-09-01 16:46:10 -0400

    Bunch of work done. Catching up after long hiatus.

--- wp-content/themes/nth/sidebar.php
+++ wp-content/themes/nth/sidebar.php
@@ -2,6 +2,8 @@
 <aside class="sidebar" role="complementary">
 
 	<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area')) ?>
-		
+
+	<?php get_footer(); ?>
+
 </aside>
 <!-- /sidebar -->
--- wp-content/themes/nth/single.php
+++ wp-content/themes/nth/single.php
@@ -1,27 +1,27 @@
 <?php get_header(); ?>
-	
+
 	<!-- main -->
 	<main id="main">
-	
+
 		<section class="single-post">
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		
+
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<header>
 
 					<!-- post title -->
-			        <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>    
+			        <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
 			        <!-- /post title -->
 			        <!-- post details -->
 			        <span class="author">by <?php the_author_posts_link(); ?></span>
-			        <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></time>		
+			        <time datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></time>
 					<!-- /post details -->
 
-			    </header> 
-			
+			    </header>
+
 				<!-- post thumbnail -->
 				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -37,9 +37,8 @@
 
 					<?php comments_template(); ?>
 
-					<?php the_tags("<span class='tags-header'>Tags:</span><dd class='tags'><dl>", "</dl><dl>", "</dl></dd>"); ?>
+					<?php the_tags("<dd class='tags'><dl>", "</dl><dl>", "</dl></dd>"); ?>
 
-					<span class="cats-header">Categories:</span>
 		            <dd class="categories">
 		            	<dl>
 							<?php the_category("</dl><dl>"); ?>
@@ -48,29 +47,29 @@
 
 					<?php edit_post_link(); ?>
 
-				</footer>		
-				
+				</footer>
+
 			</article>
 			<!-- /article -->
-			
+
 		<?php endwhile; ?>
-		
+
 		<?php else: ?>
-		
+
 			<!-- article -->
 			<article>
-				
+
 				<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-				
+
 			</article>
 			<!-- /article -->
-		
+
 		<?php endif; ?>
 
 		</section>
 
 	<?php get_sidebar(); ?>
-	
+
 	</main>
 	<!-- /main -->
 
