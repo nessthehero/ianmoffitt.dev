36e0dc081a7b85f180d494f2e6dd5d86e176c08d
Ian Moffitt <IMoffitt@barkleyrei.com>
2018-04-21 16:34:07 -0400

    feat: Continued integration and improvement of layout and styles

--- web/themes/nth/php/preprocess/_global.php
+++ web/themes/nth/php/preprocess/_global.php
@@ -5,3 +5,26 @@
 		build_includes($variables);
 
 	}
+
+	function nth_preprocess_block(&$variables)
+	{
+
+		$block = $variables['base_plugin_id'];
+
+		// echo '';
+
+		switch ($block) {
+
+			case 'system_branding_block':
+			case 'svglogo':
+
+				$variables['attributes']['class'][] = 'logo';
+
+				break;
+
+			default:
+				break;
+
+		}
+
+	}
