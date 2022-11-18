21eea45e554472bb2b7365d96add3430208939f0
Ian Moffitt <imoffitt@andrew.cmu.edu>
2022-11-18 15:10:51 -0500

    Fix environmental variable references.

--- docroot/sites/default/settings.remote.php
+++ docroot/sites/default/settings.remote.php
@@ -3,12 +3,12 @@
 	// @codingStandardsIgnoreFile
 
 	$databases['default']['default'] = array (
-		'database' => $_ENV['IANMOFFITT_DATABASE_DBNAME'],
-		'username' => $_ENV['IANMOFFITT_DATABASE_USERNAME'],
-		'password' => $_ENV['IANMOFFITT_DATABASE_PASSWORD'],
-		'prefix' => $_ENV['IANMOFFITT_DATABASE_PREFIX'],
-		'host' => $_ENV['IANMOFFITT_DATABASE_HOST'],
-		'port' => $_ENV['IANMOFFITT_DATABASE_PORT'],
+		'database' => $_SERVER['IANMOFFITT_DATABASE_DBNAME'],
+		'username' => $_SERVER['IANMOFFITT_DATABASE_USERNAME'],
+		'password' => $_SERVER['IANMOFFITT_DATABASE_PASSWORD'],
+		'prefix' => $_SERVER['IANMOFFITT_DATABASE_PREFIX'],
+		'host' => $_SERVER['IANMOFFITT_DATABASE_HOST'],
+		'port' => $_SERVER['IANMOFFITT_DATABASE_PORT'],
 		'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
 		'driver' => 'mysql',
 	);
