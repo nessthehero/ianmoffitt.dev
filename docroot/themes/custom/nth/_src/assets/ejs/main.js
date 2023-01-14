9bc37d3cf226b4dbe13969869f52107f0bd154ab
Ian Moffitt <imoffitt@andrew.cmu.edu>
2023-01-14 10:11:41 -0500

    More CKEditor improvements.

--- docroot/themes/custom/nth/_src/assets/ejs/main.js
+++ docroot/themes/custom/nth/_src/assets/ejs/main.js
@@ -1,16 +1,19 @@
 import grid from './modules/grid';
 import expand from './modules/expand';
+import hljs from 'highlight.js';
 
 const $ = window.$ || window.jQuery || {};
 
 const ready = () => {
 
-	console.log('hello ian 1.0.1');
+	console.log('hello ian 1.0.2');
 
 	grid.init();
 	expand.init();
 
 	$(document).foundation();
+
+	hljs.highlightAll();
 
 };
 
