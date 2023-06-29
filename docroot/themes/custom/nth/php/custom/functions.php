4ef58c67ece6e60868070b9517b152596c86748a
Ian Moffitt <imoffitt@andrew.cmu.edu>
2023-06-29 09:40:09 -0400

    Update to Drupal 10.

--- docroot/themes/custom/nth/php/custom/functions.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-  // Theme specific functions not applicable to helper module(s).
--- docroot/themes/custom/nth/php/partials/accessnav.php
+++
@@ -1,5 +0,0 @@
-<div class="access-nav" role="navigation">
-	<a class="show-on-focus" href="#js-global-header">Jump to Header</a>
-	<a class="show-on-focus" href="#main">Jump to Main Content</a>
-	<a class="show-on-focus" href="#js-global-footer">Jump to Footer</a>
-</div>
