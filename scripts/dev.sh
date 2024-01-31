8d64c0e7a4a0b04606e269eb14f58cef56cc1008
Ian Moffitt <imoffitt@andrew.cmu.edu>
2024-01-31 07:19:44 -0500

    adjustment to deploy script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,12 +4,6 @@ ls -la
 # View Git status
 git status
 
-# Remove all unknown, non-ignored files
-git clean -df
-
-# Reset HEAD and discard any local changes to tracked files
-git reset --hard
-
 # NPM
 npm cache verify
 npm install
