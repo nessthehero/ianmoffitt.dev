419e31dcbdd05bd4418a50262f4a324d5e0ad5ef
Ian Moffitt <imoffitt@andrew.cmu.edu>
2024-02-02 09:30:00 -0500

    debugging img

--- src/lib/compressimages.js
+++ src/lib/compressimages.js
@@ -1,8 +1,8 @@
 const compress_images = require("compress-images");
 const root = __dirname + '/..';
 
-const INPUT_path_to_your_images = root + "/src/assets/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
-const OUTPUT_path = root + "/src/public/img/";
+const INPUT_path_to_your_images = root + "/assets/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
+const OUTPUT_path = root + "/public/img/";
 
 compress_images(INPUT_path_to_your_images, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
 	{ jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
