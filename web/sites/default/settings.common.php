f2122dd533ddbf0137b5b6ae9a9472c4af54f5aa
Ian Moffitt <imoffitt@andrew.cmu.edu>
2024-06-12 07:59:13 -0400

    Fix settings discrepencies.

--- web/sites/default/settings.common.php
+++ web/sites/default/settings.common.php
@@ -70,53 +70,6 @@
 	$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 	/**
-	 * Trusted host configuration.
-	 *
-	 * Drupal core can use the Symfony trusted host mechanism to prevent HTTP
-	 * Host header spoofing.
-	 *
-	 * To enable the trusted host mechanism, you enable your allowable hosts
-	 * in $settings['trusted_host_patterns']. This should be an array of
-	 * regular
-	 * expression patterns, without delimiters, representing the hosts you
-	 * would
-	 * like to allow.
-	 *
-	 * For example:
-	 *
-	 * @code
-	 * $settings['trusted_host_patterns'] = [
-	 *   '^www\.example\.com$',
-	 * ];
-	 * @endcode
-	 * will allow the site to only run from www.example.com.
-	 *
-	 * If you are running multisite, or if you are running your site from
-	 * different domain names (eg, you don't redirect http://www.example.com to
-	 * http://example.com), you should specify all of the host patterns that
-	 * are
-	 * allowed by your site.
-	 *
-	 * For example:
-	 * @code
-	 * $settings['trusted_host_patterns'] = [
-	 *   '^example\.com$',
-	 *   '^.+\.example\.com$',
-	 *   '^example\.org$',
-	 *   '^.+\.example\.org$',
-	 * ];
-	 * @endcode
-	 * will allow the site to run off of all variants of example.com and
-	 * example.org, with all subdomains included.
-	 */
-
-	$settings['trusted_host_patterns'] = [
-		'^ianmoffitt\.dev$',
-		'^.+\.ianmoffitt\.dev$',
-		'^www\.ianmoffitt\.dev$',
-	];
-
-	/**
 	 * The default list of directories that will be ignored by Drupal's file
 	 * API.
 	 *
@@ -182,10 +135,8 @@
 	$settings['config_sync_directory'] = '../config/default';
 	$config['system.logging']['error_level'] = 'verbose';
 
-	if (isset($_SERVER['IS_DEV']) || str_contains(__DIR__, 'dev.ianmoffitt.dev')) {
+	if (isset($_SERVER['IS_DEV']) && str_contains(__DIR__, 'dev.ianmoffitt.dev')) {
 		include __DIR__ . "/settings.dev.php";
-	}
-
-	if (str_contains(__DIR__, 'ianmoffitt.dev') && !str_contains(__DIR__, 'dev.ianmoffitt.dev')) {
+	} elseif (str_contains(__DIR__, 'ianmoffitt.dev') && !str_contains(__DIR__, 'dev.ianmoffitt.dev')) {
 		include __DIR__ . "/settings.production.php";
 	}
--- web/sites/default/settings.production.php
+++ web/sites/default/settings.production.php
@@ -12,3 +12,52 @@
 		'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
 		'driver' => 'mysql',
 	);
+
+	/**
+	 * Trusted host configuration.
+	 *
+	 * Drupal core can use the Symfony trusted host mechanism to prevent HTTP
+	 * Host header spoofing.
+	 *
+	 * To enable the trusted host mechanism, you enable your allowable hosts
+	 * in $settings['trusted_host_patterns']. This should be an array of
+	 * regular
+	 * expression patterns, without delimiters, representing the hosts you
+	 * would
+	 * like to allow.
+	 *
+	 * For example:
+	 *
+	 * @code
+	 * $settings['trusted_host_patterns'] = [
+	 *   '^www\.example\.com$',
+	 * ];
+	 * @endcode
+	 * will allow the site to only run from www.example.com.
+	 *
+	 * If you are running multisite, or if you are running your site from
+	 * different domain names (eg, you don't redirect http://www.example.com to
+	 * http://example.com), you should specify all of the host patterns that
+	 * are
+	 * allowed by your site.
+	 *
+	 * For example:
+	 * @code
+	 * $settings['trusted_host_patterns'] = [
+	 *   '^example\.com$',
+	 *   '^.+\.example\.com$',
+	 *   '^example\.org$',
+	 *   '^.+\.example\.org$',
+	 * ];
+	 * @endcode
+	 * will allow the site to run off of all variants of example.com and
+	 * example.org, with all subdomains included.
+	 */
+
+	$settings['trusted_host_patterns'] = [
+		'^ianmoffitt\.dev$',
+		'^.+\.ianmoffitt\.dev$',
+		'^www\.ianmoffitt\.dev$',
+	];
+
+	$settings['rebuild_access'] = FALSE;
